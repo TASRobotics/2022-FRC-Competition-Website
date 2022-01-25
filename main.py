621f229eca39cbcc3d687912d4abdd83a472d038
MattK12 <45025294+22matthewk@users.noreply.github.com>
2022-01-25 11:08:39 +0800

    new version

--- main.py
+++ main.py
@@ -8,13 +8,29 @@ app = create_app()
 def home():
     return render_template('home.html')
 
-@app.route('/scout')
+@app.route('/scout', methods=['GET', 'POST'])
 def scout():
     return render_template('scout.html')
+
+@app.route('/input')
+def input():
+    return render_template('input.html')
 
 @app.route('/data')
 def data():
     return render_template('data.html')
+
+@app.route('/autonomous')
+def auton():
+    return render_template('auton.html')
+
+@app.route('/teleop')
+def teleop():
+    return render_template('teleop.html')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
