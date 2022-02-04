025897119c9c29a01493af117ac47cd2ba5d7416
MattK12 <45025294+22matthewk@users.noreply.github.com>
2022-02-04 19:18:02 +0800

    new version

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, Response
 from .models import Scout
 from . import db
+import csv
 
 auth = Blueprint('auth', __name__)
 
@@ -9,11 +10,11 @@ auth = Blueprint('auth', __name__)
 def home():
     return render_template('home.html')
 
+# Data
 @auth.route('/data')
 def data():
     try:
         data = Scout.query.all()
-        print(data)
         return render_template('data.html', data=data)
     except Exception as e:
         # e holds description of the error
@@ -21,10 +22,9 @@ def data():
         hed = '<h1>Something is broken.</h1>'
         return hed + error_text
 
+# Scouting
 @auth.route('/scout', methods=['GET', 'POST'])
 def attempt():
-    data = request.form
-    print(data)
     if request.method == 'POST':
         team = request.form.get('team')
         round = request.form.get('round')
@@ -41,7 +41,7 @@ def attempt():
         auton_lower_missed = request.form.get('auton_lower_missed')
         auton_lower_unreliable = request.form.get('auton_lower_unreliable')
 
-        # teleop
+        # Teleop
         tele_upper_in = request.form.get('tele_upper_in')
         tele_upper_missed = request.form.get('tele_upper_missed')
         tele_upper_unreliable = request.form.get('tele_unreliable_upper')
@@ -66,3 +66,25 @@ def attempt():
         db.session.commit()
         
     return render_template('scout.html')
+
+# Download the database
+@auth.route('/download')
+def download():
+    data = Scout.query.all()
+
+    # Store it as csv 
+    with open(r'C:\Users\22matthewk\Desktop\data.csv', 'w') as s_key:
+        csv_out = csv.writer(s_key)
+
+        # Horizontal labels
+        csv_out.writerow(["Team", "Round", "Alliance", "Taxi", "A_Upper_In", "A_Upper_Missed", "A_Upper_Unreliable", "A_Lower_In", "A_Lower_Missed", "A_Lower_Unreliable",
+                         "T_Upper_In", "T_Upper_Missed", "T_Upper_Unreliable", "T_Lower_In", "T_Lower_Missed", "T_Lower_Unreliable", "Hang", "Cargo", "Hangar"])
+        
+        # Database data
+        data = db.session.query(Scout.team, Scout.round, Scout.alliance, Scout.taxi, Scout.auton_upper_in, Scout.auton_upper_missed, 
+                                Scout.auton_upper_unreliable, Scout.auton_lower_in, Scout.auton_lower_missed, Scout.auton_lower_unreliable, 
+                                Scout.tele_upper_in, Scout.tele_upper_missed, Scout.tele_upper_unreliable, Scout.tele_lower_in, 
+                                Scout.tele_lower_missed, Scout.tele_lower_unreliable, Scout.hang, Scout.cargo_bonus, Scout.hangar_bonus)
+        for i in data:
+            csv_out.writerow(i)
+    return render_template('data.html',data=data)
