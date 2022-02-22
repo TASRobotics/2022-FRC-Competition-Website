97ac6cffbdb4ef89aa71466875de68897487e077
williamhCode <83525937+williamhCode@users.noreply.github.com>
2022-02-22 10:38:58 +0800

    update

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from website import create_app
-from replit import web
+# from replit import web
 
 app = create_app()
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    # app.run(debug=True)
+    app.run(debug=True)
     
     # for replit hosting
-    web.run(app, debug=True)
-=======
-    app.run(debug=True)
->>>>>>> origin/master
+    # web.run(app, debug=True)
