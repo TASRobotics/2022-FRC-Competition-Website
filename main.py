df41b0223c438e9916b1ce3b2082b526791b02e0
MattK12 <45025294+22matthewk@users.noreply.github.com>
2022-03-02 15:09:25 +0800

    new version

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from website import create_app
-# from replit import web
-
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    # for replit hosting
-    # web.run(app, debug=True)
